e79f9f675a65055219867067abed626ab5bb7fcb
Palyfight <chrisbama92@gmail.com>
2023-01-29 12:36:00 -0500

    initial commit

---
+++ src/main/java/com/github/palyfight/superheatbot/SuperheatbotApplication.java
@@ -0,0 +1,13 @@
+package com.github.palyfight.superheatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/palyfight/superheatbot/SuperheatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.palyfight.superheatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
